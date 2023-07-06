abcead6870565c263e6dd9c9b3b1e47cc577c5ef
ksaidi <ksaidi@cts.asal.com>
2023-07-06 11:57:13 +0100

    init  commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+
+    return render_template('home.html')
+
+@app.route('/acount/<name>')
+def profile(name):
+
+    return render_template('profile.html' ,aaaa=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
